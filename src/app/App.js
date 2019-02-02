7de30dea0232c82ac9c79671b06adb2bc5b8b69e
Leo Taylor <leotaylor723@gmail.com>
2019-02-02 10:01:54 -0600

    routing

--- src/app/App.js
+++ src/app/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import {Route, BrowserRouter, Redirect, Switch} from 'react-router-dom';
+
 import Login from '../components/Login/Login';
 import Navbar from '../components/navbar/navbar';
 import Home from '../components/home/home';
@@ -6,14 +8,74 @@ import Admin from '../components/admin/admin';
 
 import './App.css';
 
+const PrivateRoute = ({ component: Component, authed, ...rest}) => {
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        authed === true ? (
+          <Component {...props} />
+        ) : (
+          <Redirect
+            to={{ pathname: '/login', state: {from: props.location}}}
+          />
+        )
+      }
+    />
+  );
+};
+
+const PublicRoute = ({ component: Component, authed, ...rest}) => {
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        authed === false ? (
+          <Component {...props} />
+        ) : (
+          <Redirect
+            to={{ pathname: '/home', state: {from: props.location}}}
+          />
+        )
+      }
+    />
+  );
+};
+
 class App extends Component {
+  state = {
+    authed: false,
+  }
   render () {
     return (
       <div className="App">
-        <Navbar />
-        <Login />
-        <Home />
-        <Admin />
+        <BrowserRouter>
+          <div>
+            <Navbar />
+            <div className="container">
+              <div className="row">
+                <Switch>
+                  <Route path='/' exact component={Home} />
+                  <PrivateRoute
+                    path='/admin'
+                    authed={this.state.authed}
+                    component={Admin}
+                  />
+                  <PublicRoute
+                    path="/login"
+                    authed={this.state.authed}
+                    component={Login}
+                  />
+                  <PublicRoute
+                    path="/home"
+                    authed={this.state.authed}
+                    component={Home}
+                  />
+                </Switch>
+              </div>
+            </div>
+          </div>
+        </BrowserRouter>
       </div>
     );
   }
