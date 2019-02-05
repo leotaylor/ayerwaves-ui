8401be96793340aa18af45dd2a3ea3741402bf12
Leo Taylor <leotaylor723@gmail.com>
2019-02-04 18:20:43 -0600

    attempt was made for nice looking nav dropdown

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import artistRequest from '../../apiRequest/artists';
+// import { NavDropdown } from 'react-bootstrap';
 
 import './navbar.css';
 
