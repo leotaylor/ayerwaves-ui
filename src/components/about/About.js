5471c882b949b57ab8fc60d5acc5384ec573a311
Leo Taylor <leotaylor723@gmail.com>
2019-02-13 12:00:00 -0600

    press

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -10,10 +10,11 @@ class About extends React.Component {
       <div className="aboutDiv">
         <div className="col-sm-6 text-left">
           <div className="aboutus">
-            <h1>AyerWaves History</h1>
+            <h1 className="history">AyerWaves History</h1>
             <p className="writing">
             AyerWaves was founded by Joshua Ayers in 2012 as "Josh's Underground".  It started as a radio show on WHAY 98.3FM in Whitley City, Kentucky. The two hour time slot was spent showcasing independent musicians from around the world.<br/>
-    In 2015 the first AyerWaves live started as "Josh's Underground Acoustic Summer Concert Series". The event was held at Josh's yard in Oneida, Tennessee. The series consisted of seven concerts on 3 acres of land throughout the summer showcasing touring independent musicians and pairing them with a local performer. By the seventh concert the events were getting to large, so in 2015 Josh met with the owners of the Possum-Trot Western Themed Mountain Resort about bringing his event to their location. The two sides worked out a deal, and the AyerWaves became a small town Music Festival. The 2016 event known as the AyerWaves "1" was held on 46 acres with one stage outside in the town of Possum-Trot. The event had 20 performances over the two days. In 2017 AyerWaves "2"  rapidly expanded to 191 acres with four stages and 58 performances over four days headlined by Darrell Scott and Home Grown Head. AyerWaves "3" was headlined by Keller Williams and Emi Sunshine. AyerWaves "4" will surely entertain as the event continues to grow, and moves it primary location to middle Tennessee!<br/>
+    In 2015 the first AyerWaves live started as "Josh's Underground Acoustic Summer Concert Series". The event was held at Josh's yard in Oneida, Tennessee. The series consisted of seven concerts on 3 acres of land throughout the summer showcasing touring independent musicians and pairing them with a local performer. By the seventh concert the events were getting to large, so in 2015 Josh met with the owners of the Possum-Trot Western Themed Mountain Resort about bringing his event to their location. The two sides worked out a deal, and the AyerWaves became a small town Music Festival. <br/>
+    The 2016 event known as the AyerWaves "1" was held on 46 acres with one stage outside in the town of Possum-Trot. The event had 20 performances over the two days. In 2017 AyerWaves "2"  rapidly expanded to 191 acres with four stages and 58 performances over four days headlined by <a href="http://www.darrellscott.com/" className="rainbow">Darrell Scott</a> and Home Grown Head. AyerWaves "3" was headlined by <a href="https://kellerwilliams.net/" className="rainbow">Keller Williams </a> and <a href="http://theemisunshine.com/" className="rainbow">Emi Sunshine</a>. AyerWaves "4" will surely entertain as the event continues to grow, and moves it primary location to middle Tennessee!<br/>
     AyerWaves Entertainment now helps organize and create for private, corporate, and community events. Joshua spends most of his time booking talent in Tennessee and Kentucky, and keeps a focus on sharing independent artist traveling the country. You can still hear the radio show every Monday night at 7pm EST on WHAY 98.3FM.
             </p>
           </div>
@@ -21,6 +22,31 @@ class About extends React.Component {
         <div className="chairman col-sm-6 offset-sm-3">
           <img className="josh" src={require('../../images/josh.png')} alt="josh"></img>
           <p>Joshua Ayers - Founder/CEO/Chairman</p>
+        </div>
+        <div className="row">
+          <div className="col-sm-12">
+            <h1>Press</h1>
+            <div className="col-sm-6">
+              <img className="mag" src={require('../../images/magcover.jpg')} alt="magcover"></img>
+            </div>
+            <div className="col-sm-6">
+              <img className="mag" src={require('../../images/magin.jpg')} alt="magin"></img>
+            </div>
+          </div>
+        </div>
+        <div className='row'>
+          <div className='col-sm-12'>
+            <a href="https://www.ionindiemagazine.com/"><h2>IonIndie Magazine: </h2></a>
+            <div className="col-sm-4">
+              <img className="ind" src={require('../../images/ho.jpg')} alt="magin"></img>
+            </div>
+            <div className="col-sm-4">
+              <img className="ind" src={require('../../images/suga.jpg')} alt="magin"></img>
+            </div>
+            <div className="col-sm-4">
+              <img className="ind" src={require('../../images/mem.jpg')} alt="magin"></img>
+            </div>
+          </div>
         </div>
       </div>
     );
